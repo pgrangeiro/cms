f1bcf6d0199436be303333e0d7e977ba0bbd9691
Paula Grangeiro <pgrangeiro.dev@gmail.com>
2016-05-07 23:45:58 -0300

    Adding lib setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='CMS',
+    version='0.0',
+    description='Python CMS lib',
+    author='Paula Grangeiro',
+    author_email='contato@paulagrangeiro.com.br',
+    url='https://github.com/pgrangeiro/cms',
+    license='GNU 3.0',
+    extra_require={
+        'test': 'mock',
+    },
+)
