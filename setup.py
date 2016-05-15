46a791e67560588a9c899f5c9140e16dc9dd9820
Paula Grangeiro <pgrangeiro.dev@gmail.com>
2016-05-15 19:16:53 -0300

    Moving files and renaming GitHub repository

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 
 setup(
-    name='CMS',
+    name='PyCMS',
     version='0.0',
     description='Python CMS lib',
     author='Paula Grangeiro',
     author_email='contato@paulagrangeiro.com.br',
-    url='https://github.com/pgrangeiro/cms',
+    url='https://github.com/pgrangeiro/pycms',
     license='GNU 3.0',
     extras_require={
         'test': ['mock'],
